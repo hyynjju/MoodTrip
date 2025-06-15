70d881eca2f068b157983332d0b4f149bf35fc32
hjuump <butter0488@gmail.com>
2025-06-15 18:10:52 +0900

    feat: JSON 파일 내용 수정

--- MoodTrip/Models/Place.swift
+++ MoodTrip/Models/Place.swift
@@ -10,7 +10,12 @@ struct Place: Codable {
     let latitude: Double
     let longitude: Double
     let scores: [String: Int]
-    
+    let tags: [String]
+    let recommendedDuration: String
+    let bestWith: String
+    let address: String
+    let detailedDescription: String
+
     /// CLLocationCoordinate2D로 변환된 위치 정보
     var coordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
