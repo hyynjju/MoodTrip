3b71f19e72119e30e69a9d5e547837df61c85996
hjuump <butter0488@gmail.com>
2025-06-17 13:54:39 +0900

    feat: 탭바 항목 수정

--- MoodTrip/ViewControllers/MainTabBarController.swift
+++ MoodTrip/ViewControllers/MainTabBarController.swift
@@ -7,17 +7,13 @@ class MainTabBarController: UITabBarController {
         let mainVC = MainViewController()
         mainVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
 
-        let mapVC = MapViewController()
-        mapVC.tabBarItem = UITabBarItem(title: "Map", image: UIImage(systemName: "map"), tag: 1)
-
-        let savedVC = BookmarkViewController()
+        let savedVC = SavedViewController()
         savedVC.tabBarItem = UITabBarItem(title: "Saved", image: UIImage(systemName: "bookmark"), tag: 2)
 
         let mainNav = UINavigationController(rootViewController: mainVC)
-        let mapNav = UINavigationController(rootViewController: mapVC)
         let savedNav = UINavigationController(rootViewController: savedVC)
 
-        viewControllers = [mainNav, mapNav, savedNav]
+        viewControllers = [mainNav, savedNav]
         tabBar.barTintColor = .black
         tabBar.tintColor = UIColor(named: "PointColor") ?? .systemBlue
     }
