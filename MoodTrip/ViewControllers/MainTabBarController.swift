f3654d9672827382513ea5517ce63327baf7ef2f
hjuump <butter0488@gmail.com>
2025-06-18 01:38:52 +0900

    style: 탭바 스타일 수정

--- MoodTrip/ViewControllers/MainTabBarController.swift
+++ MoodTrip/ViewControllers/MainTabBarController.swift
@@ -5,20 +5,24 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         let mainVC = MainViewController()
-        mainVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        mainVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house.fill"), selectedImage: UIImage(systemName: "house.fill"))
 
         let savedVC = SavedViewController()
-        savedVC.tabBarItem = UITabBarItem(title: "Saved", image: UIImage(systemName: "bookmark"), tag: 2)
+        savedVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bookmark.fill"), selectedImage: UIImage(systemName: "bookmark.fill"))
 
         let mainNav = UINavigationController(rootViewController: mainVC)
         let savedNav = UINavigationController(rootViewController: savedVC)
         
         let insightVC = InsightViewController()
-        insightVC.tabBarItem = UITabBarItem(title: "Insights", image: UIImage(systemName: "chart.bar"), tag: 3)
+        insightVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "chart.bar.fill"), selectedImage: UIImage(systemName: "chart.bar.fill"))
         let insightNav = UINavigationController(rootViewController: insightVC)
 
         viewControllers = [mainNav, savedNav, insightNav]
         tabBar.barTintColor = .black
-        tabBar.tintColor = UIColor(named: "PointColor") ?? .systemBlue
+        tabBar.tintColor = UIColor(red: 0.45, green: 0.76, blue: 1.0, alpha: 1.0) // #73C2FF
+        
+        for item in tabBar.items ?? [] {
+            item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 300)
+        }
     }
 }
