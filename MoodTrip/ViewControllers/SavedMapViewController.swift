c375400f103b18d6047f7a18aece5da1b47437d0
hjuump <butter0488@gmail.com>
2025-06-17 16:32:39 +0900

    feat: 맵뷰 스케일 수정

--- MoodTrip/ViewControllers/SavedMapViewController.swift
+++ MoodTrip/ViewControllers/SavedMapViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 import MapKit
 
 class SavedMapViewController: UIViewController {
+    
+    var place: Place?
+    
     private let mapView: MKMapView = {
         let mapView = MKMapView(frame: .zero)
         mapView.mapType = .satelliteFlyover
@@ -16,17 +19,27 @@ class SavedMapViewController: UIViewController {
         mapView.pointOfInterestFilter = .includingAll
         return mapView
     }()
-    var place: Place?
+    private var bookmarkedPlaces: [Place] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         setupMapView()
+        loadBookmarkedPlaces()
+        displayBookmarkedPlacesOnMap()
         if #available(iOS 15.0, *) {
             var globeCamera = MKMapCamera()
-            globeCamera.centerCoordinate = CLLocationCoordinate2D(latitude: 20.0, longitude: 0.0)
+            globeCamera.centerCoordinate = CLLocationCoordinate2D(latitude: 35.8, longitude: 127.5)
             globeCamera.pitch = 0
-            globeCamera.altitude = 30000000 // High altitude for globe view
+            globeCamera.altitude = 30000000 // High altitude for initial globe view
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                let focusedCamera = MKMapCamera()
+                focusedCamera.centerCoordinate = CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780) // Example coordinate (Seoul)
+                focusedCamera.altitude = 3000000
+                UIView.animate(withDuration: 2.0, animations: {
+                    self.mapView.setCamera(focusedCamera, animated: true)
+                })
+            }
             mapView.setCamera(globeCamera, animated: false)
         }
     }
@@ -40,5 +53,18 @@ class SavedMapViewController: UIViewController {
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+
+    private func loadBookmarkedPlaces() {
+        bookmarkedPlaces = BookmarkManager.getBookmarkedPlaces()
+    }
+
+    private func displayBookmarkedPlacesOnMap() {
+        for place in bookmarkedPlaces {
+            let annotation = MKPointAnnotation()
+            annotation.title = place.name
+            annotation.coordinate = CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
+            mapView.addAnnotation(annotation)
+        }
     }
 }
